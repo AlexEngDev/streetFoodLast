de41ace6b140a1f85491d636a3ab84b10687d63c
Alex <alexandr1991xxx@gmail.com>
2023-05-30 14:11:52 +0200

    commit_4

--- src/app/pizze/pizze.component.ts
+++ src/app/pizze/pizze.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pizza } from '../models/pizza.model';
+import { PizzaService } from '../servizi/pizza.service';
 
 @Component({
   selector: 'app-pizze',
   templateUrl: './pizze.component.html',
   styleUrls: ['./pizze.component.css']
 })
-export class PizzeComponent {
+export class PizzeComponent implements OnInit{
+
+  constructor(private pizzaService: PizzaService) {
+  }
+
+
+  receivedPizza: Pizza[];
+
+
+  ngOnInit(): void {
+  this.receivedPizza = this.pizzaService.listaPizze
+  }
 
 }
